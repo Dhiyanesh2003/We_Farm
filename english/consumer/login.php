c8508927be97d7adeb6f5db62f9bd17963595646
Dhiyanesh2003 <dhiyanesh7338942092@gmail.com>
2023-01-12 12:41:04 +0530

    modified

--- english/consumer/login.php
+++ english/consumer/login.php
@@ -1,24 +1,24 @@
 <?php   
-    
-    $host = "sql110.epizy.com";  
-    $user = "epiz_31427880";  
-    $password = "5Jr6wSCXHg";  
-    $db_name = "epiz_31427880_demo"; 
+	session_start();
+    $host = "localhost";  
+    $user = "root";  
+    $password = "";
+    $db_name = "organi5"; 
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
     }
     else{
         
-        $user_id = $_POST['user_id']; 
-        $password = $_POST['password'];
-        $sql = "select * from loginform where User = '".$user_id."' and Pass = '".$password."'";  
+        $_SESSION['user_id'] = $_POST['user_id']; 
+        $_SESSION['password'] = $_POST['password'];
+        $sql = "select * from loginform where username = '".$_SESSION['user_id']."' and pass = '".$_SESSION['password']."'";  
         $result = mysqli_query($con, $sql);  
         $row = mysqli_fetch_assoc($result);  
         $count = mysqli_num_rows($result);
         if($count == 1){  
-            
-            include "consumer_home.html";
+			echo "<script> location.href='consumer_home.php'; </script>";
+			exit;
         }  
         else{  
             echo "<h1> Login failed. Invalid username or password.</h1>";  
--- english/consumer/register.php
+++ english/consumer/register.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql110.epizy.com";
-$username = "epiz_31427880";
-$password = "5Jr6wSCXHg";
-$dbname = "epiz_31427880_demo";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "organi5";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -10,15 +10,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$USER_ID= "123";
 $USERNAME= $_POST['USERNAME']; 
 $PASSWORD= $_POST['PASSWORD'];
 $NUMBER= $_POST['NUMBER'];
 $EMAIL= $_POST['EMAIL'];
 $ADDRESS= $_POST['ADDRESS'];
-$P1=$_POST['P1'];
-$sql = "INSERT INTO register VALUES ('".$USERNAME."','".$PASSWORD."','".$P1."','".$NUMBER."','".$EMAIL."','".$ADDRESS."')";
+$sql = "INSERT INTO loginform VALUES ('".$USER_ID."','".$USERNAME."','".$PASSWORD."','".$NUMBER."','".$EMAIL."','".$ADDRESS."')";
 if ($conn->query($sql) === TRUE) {
-  include "consumer_home.html";
+  include "consumer_login.html";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
